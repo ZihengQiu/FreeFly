0055757dd84826888fa8bb6a88114d9ea3e7571e
ZaynQiu <1500933961@qq.com>
2023-10-07 15:55:51 +0800

    deleted .git in ucos2

---
+++ os_version/ucos2/Cfg/includes.h
@@ -0,0 +1,14 @@
+#ifndef __INCLUDES_H
+#define __INCLUDES_H
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+#include "ucos_ii.h"
+#include "os_cpu.h"
+#include "os_cfg.h"
+#include "os_trace.h"
+
+#endif
